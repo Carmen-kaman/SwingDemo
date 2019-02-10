556933858aa0278b46cc4682e4db4351942fd19a
Carmen-kaman <cahejiamin@163.com>
2019-02-10 13:51:57 -0500

    1

---
+++ src/model.java
@@ -0,0 +1,17 @@
+import java.util.Observable;
+
+public class model extends Observable {
+
+    public String name = new String(" first name");
+
+    public void setName(String s){ //更新数据
+
+        this.name = s;
+
+        System.out.println("m name "+this.name);
+
+        this.setChanged();
+        this.notifyObservers( );// 通知view 数据发生更改；
+
+    }
+}
---
+++ src/view.java
@@ -0,0 +1,43 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class view extends JFrame implements Observer {
+
+    private JPanel panel;
+    private JButton button;
+    public model m;
+
+    public view() {
+
+
+        super("Set Name");
+        setSize(600,500);
+        this.setLayout(new BorderLayout());
+        panel = new JPanel();
+        button = new JButton("set");
+        panel.add(button);
+        this.add(panel, BorderLayout.SOUTH);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) { // 获取到model 数据发生更新的消息；
+            System.out.println("view receive information");
+            System.out.println("this is model`s name："+ m.name);
+            System.out.println("finish set name");
+    }
+
+    public void addController(controller c){
+        System.out.println("controller is listener");
+        this.button.addActionListener(c);// button 的监听放在了 controller
+    }
+
+    public void addModel(model m){
+        this.m = m;
+    }
+}
